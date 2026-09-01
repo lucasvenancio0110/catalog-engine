f59a6283773225820d57bfd7ee742c312d602965
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-09-01 12:38:47 -0300

    M9B: load infinite scroll presentation

--- src/entry.js
+++ src/entry.js
@@ -9,5 +9,6 @@ import './storefront/experience-motion.css';
 import './storefront/catalog-mobile-density.css';
 import './storefront/team-page-filter-density.css';
 import './storefront/mobile-quick-view.css';
+import './storefront/catalog-infinite-scroll.css';
 import './storefront/luxury-mobile.js';
 import './storefront/mobile-shell.js';
